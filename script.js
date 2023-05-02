d9c7c0288b41bf9bd086545ca8b678d6007362c8
sungjin <ysoon0912@gmail.com>
2023-05-02 14:52:00 +0900

    Add createDiv, markBlack function

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 const container = document.querySelector('.container');
-container.textContent = "hey man"
+
+let rowNum = 64;
+
+function createDiv(row){
+    for(let i = 0; i<row; i++){
+        const flexContainer = document.createElement('div');
+        flexContainer.style.display = 'flex';
+        flexContainer.style.width = '100%';
+        flexContainer.style.flex = 1;
+        for(let j = 0; j<row; j++){
+            const div = document.createElement('div');
+            div.classList.add('square');
+            div.style.border = '1px solid black';
+            div.style.flex = 1;
+            flexContainer.appendChild(div);
+        }
+        container.appendChild(flexContainer);
+    }
+}
+
+createDiv(rowNum);
+
+function markBlack(e){
+    e.target.style.backgroundColor = 'black';
+}
+
+const squares = Array.from(document.querySelectorAll('.square'));
+squares.forEach((item) => item.addEventListener('mouseover', markBlack));
